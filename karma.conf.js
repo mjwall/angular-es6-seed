8802b5042ae2978af6c8ae6e980319fffb8b5ee5
Michael Wall <mjwall@gmail.com>
2017-04-10 16:54:51 -0400

    karma is closer
    Can't load css

--- karma.conf.js
+++ karma.conf.js
@@ -26,8 +26,14 @@ module.exports = function (config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {
+      ['./app/app.js']: ['webpack'],
+      ['./app/**/*-test.js']: ['webpack']
+    },
 
+    webpackPreprocessor: {
+      configPath: './webpack.config.js'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
