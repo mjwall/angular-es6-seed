9d21bf46645bfdb69ed57d59bf764d160ca230b2
Michael Wall <mjwall@gmail.com>
2017-04-09 20:58:01 -0400

    Closer to start and build
    getting a weird error
    "Uncaught ReferenceError: webpackJsonp is not defined"

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,9 @@
 import angular from 'angular'
 import ngRoute from 'angular-route'
-import {View1Controller} from './view1-controller'
+import {
+  View1Controller
+} from './view1-controller'
+
 
 export default angular
   .module('myApp.view1', [
@@ -8,7 +11,7 @@ export default angular
   ])
   .config(($routeProvider) => {
     $routeProvider.when('/view1', {
-      templateUrl: 'view1/view1.html',
+      templateUrl: './view1/view1.html',
       controller: View1Controller
     })
   })
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,118 @@
 var path = require('path')
 var webpack = require('webpack')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  devtool: process.env.NODE_ENV ? 'source-map' : 'eval',
-  cache: true,
-  entry: './app/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    sourceMapFilename: '[name].map',
-    chunkFilename: '[id].chunk.js'
-  },
-  module: {
-    rules: [{
+module.exports = function (env) {
+  return {
+    context: path.resolve(__dirname, 'app'),
+    entry: {
+      app: './app.js',
+      vendor: ['angular', 'angular-route']
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    module: {
+      rules: [{
+        test: /\.(css|png|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: '',
+          name: "[path][name].[ext]"
+        }
+      }, {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }, {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['es2015', {
+                modules: false
+              }]
+            ],
+            plugins: ['syntax-dynamic-import']
+          }
+        }]
+      }],
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        // split up your app code from the libraries, must match entry name
+        // assumes your vendor imports exist in the node_modules directory
+        name: 'vendor',
+        minChunks: function (module) {
+          return module.context && module.context.indexOf('node_modules') !== -1;
+        }
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        // CommonChunksPlugin will now extract all the common modules from vendor and app bundles
+        // If there are no common modules vendor and app, manifest will only have runtime code
+        name: 'manifest'
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'index-template.html'
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'view1/view1.html',
+        template: 'view1/view1.html'
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'view2/view2.html',
+        template: 'view2/view2.html'
+      })
 
-      use: [
-         {
-           test: /\.js$/,
-           exclude: /(node_modules)/,
-           loader: "babel-loader",
-           options: {
-               presets: ["es2015"]
-           } 
-         }
     ]
-    }]
-  },
-  resolve: {
-    modules: [
-       path.resolve(__dirname, "app"),
-      'node_modules'
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true, 
-    historyApiFallback: true, 
-    //hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor'],
-      minChunks: Infinity
-    }),
-    new CopyWebpackPlugin([{
-      context: path.resolve(__dirname, 'app'),
-      from: '**/*.{html,css}'
-    }]),
-    new webpack.NoErrorsPlugin()
-  ]
+    // devtool: process.env.NODE_ENV ? 'source-map' : 'eval',
+    // cache: true,
+    // entry: './app/app.js',
+    // output: {
+    //   path: path.resolve(__dirname, 'dist'),
+    //   filename: '[name].js',
+    //   sourceMapFilename: '[name].map',
+    //   chunkFilename: '[id].chunk.js'
+    // },
+    // module: {
+    //   rules: [{
+
+    //     use: [{
+    //       test: /\.js$/,
+    //       exclude: /(node_modules)/,
+    //       loader: "babel-loader",
+    //       options: {
+    //         presets: ["es2015"]
+    //       }
+    //     }]
+    //   }]
+    // },
+    // resolve: {
+    //   modules: [
+    //     path.resolve(__dirname, "app"),
+    //     'node_modules'
+    //   ]
+    // },
+    // devServer: {
+    //   contentBase: path.join(__dirname, 'dist'),
+    //   compress: true,
+    //   historyApiFallback: true,
+    //   //hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    // },
+    // plugins: [
+    //   new webpack.optimize.CommonsChunkPlugin({
+    //     name: ['app', 'vendor'],
+    //     minChunks: Infinity
+    //   }),
+    //   new CopyWebpackPlugin([{
+    //     context: path.resolve(__dirname, 'app'),
+    //     from: '**/*.{html,css}'
+    //   }]),
+    //   new webpack.NoErrorsPlugin()
+    // ]
+  }
 }
