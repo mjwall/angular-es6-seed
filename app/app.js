05717390c8d11bc48195dc804076f12ef37194cc
Michael Wall <mjwall@gmail.com>
2017-04-17 21:40:18 -0400

    better looking and working

--- app/app.js
+++ app/app.js
@@ -4,13 +4,23 @@ import "./app.css";
 import angular from "angular";
 import ngRoute from "angular-route";
 
+// native angular bootstrap components
+import collapse from "../node_modules/angular-ui-bootstrap/src/collapse";
+
 import home from "./home/home";
 import view1 from "./view1/view1";
 import view2 from "./view2/view2";
 import version from "./components/version/version";
 
 var myApp = angular
-  .module("myApp", [ngRoute, version.name, home.name, view1.name, view2.name])
+  .module("myApp", [
+    ngRoute,
+    version.name,
+    home.name,
+    view1.name,
+    view2.name,
+    collapse
+  ])
   .config(($locationProvider, $routeProvider) => {
     $locationProvider.hashPrefix("!");
     $routeProvider.otherwise({
